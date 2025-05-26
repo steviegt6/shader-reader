76646aced3514b3510558440ac0215061fa30554
Stephen T <xxlennygamerxx@gmail.com>
2025-05-26 00:00:41 -0500

    better logging

--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ namespace ShaderDecompiler;
 public static class Program {
 	public static void Main() {
 		var fxcAndXnbFiles = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.TopDirectoryOnly)
-			.Where(file => file.EndsWith(".fxc", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".xnb", StringComparison.OrdinalIgnoreCase))
-			.ToList();
+		                              .Where(file => file.EndsWith(".fxc", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".xnb", StringComparison.OrdinalIgnoreCase))
+		                              .ToList();
 
 		foreach (var file in fxcAndXnbFiles) {
 			Console.WriteLine("Reading file: " + file);
@@ -25,13 +25,27 @@ public static class Program {
 			{
 				reader.BaseStream.Seek(0, SeekOrigin.Begin);
 			}
-			
+
 			Console.WriteLine("    Is XNB: " + isXnb);
-			
+
 			var effect = isXnb ? XnbReader.ReadEffect(reader) : Effect.Read(reader);
-			
+
 			Console.WriteLine("    Parameters: " + effect.Parameters.Length);
+			{
+				foreach (var parameter in effect.Parameters) {
+					Console.WriteLine("        " + parameter);
+				}
+			}
+
 			Console.WriteLine("    Techniques: " + effect.Techniques.Length);
+			{
+				foreach (var technique in effect.Techniques) {
+					Console.WriteLine("        " + technique.Name);
+					foreach (var pass in technique.Passes) {
+						Console.WriteLine("            " + pass.Name);
+					}
+				}
+			}
 		}
 	}
 }
